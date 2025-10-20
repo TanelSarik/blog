297312af64b7da0be365db80d7625edd71cdf98f
tanel sarik <tanel.sarik@tptlive.ee>
2025-10-20 18:44:43 +0300

    add slugs to posts

--- database/migrations/2025_09_29_093222_create_posts_table.php
+++ database/migrations/2025_09_29_093222_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body')->nullable();
+            $table->string('slug');
             // $table->unsignedBigInteger('user_id');
             // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             //  $table->string('image')->nullable();
